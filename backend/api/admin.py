dfde082d71be94e1084d8acf6c8fe2e00d6c71d0
Henoker <hennybany@gmail.com>
2025-04-19 18:23:44 +0300

    complete backend api by testing

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Task
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'completed')
+
+
+admin.site.register(Task, TaskAdmin)
